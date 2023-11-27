bb80e5955b097b8c9c190c70c804647c3ccd3dfd
MD Ariful Islam Shanto <arifulislam1077@gmail.com>
2023-11-26 22:55:17 -0800

    Added a delete route for deleting single wishlish collection data

--- index.js
+++ index.js
@@ -325,6 +325,15 @@ async function run() {
 
       res.send({message : "updated status"})
     })
+
+
+    //? Delete wishlist property.
+    app.delete('/deleteWishlist/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id : new ObjectId(id)};
+      const result = await wishlistCollection.deleteOne(query);
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
